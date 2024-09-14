6f10a9d57c332d8be2ab508c0dbe855a016ca4df
shaukinkhan477 <shaukinkhan477@gmail.com>
2024-09-14 18:37:39 +0530

    Project added

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -11,44 +11,58 @@ import { Component } from '@angular/core';
 export class ProjectsComponent {
 
   project1 = 'assets/images/benProject1.jpeg';
-  project2 = 'assets/images/project-2.jpg';
-  project3 = 'assets/images/project-3.jpg';
-  project4 = 'assets/images/project-4.jpg';
+  project2 = 'assets/images/benProject-2.jpeg';
+  project3 = 'assets/images/benProject-3.png';
+  project4 = 'assets/images/benProject-4.jpg';
 
   projects:any = [
     {
       title: 'Referent technique ANGULAR',
+      employer: 'Prios',
       year: 'Nov 2023 - Present',
       image: this.project1,
       description:
-        '◼Framework and train the dev team on the ANGULAR ecosystem co-Advise management on technical choices front, tests, mobile. ◼️◼Framework Develop testing strategies and implement them. ◼️◼Framework design POCs for demos. ◼️◼Framework Carry out feasibility studies. ◼️◼Framework Refactor, rewrite and document code left by the old ones from the box. ◼️◼Framework produce projects in recent versions while maintaining the stability of the customer experience.'
+        `◼Framework and train the dev team on the ANGULAR ecosystem co-Advise management on technical choices front, tests, mobile. ◼️◼Framework Develop testing strategies and implement them. ◼️◼Framework design POCs for demos. ◼️◼Framework Carry out feasibility studies. ◼️◼Framework Refactor, rewrite and document code left by the old ones from the box. ◼️◼Framework produce projects in recent versions while maintaining the stability of the customer experience.`
         .replaceAll('◼️', '<br>'),
       technologies: ['ANGULAR 9, 11, 14', 'Rxjs', 'Jest, jasmine, Karma', 'Node.js 12, 14, 16, 18',],
     },
     {
       title: 'Software Trainer (Web-Process)',
+      employer: 'Trusted Advisors Group',
       year: 'May 2023 - Aug 2023',
       image: this.project2,
       description:
         `◼Angular Advanced
-         ◼️FrontEnd Architecture
-         ◼️Github Ci/CD
-        ◼️Bootstrapping apps with firebase.`.replaceAll('◼️', '<br>'),
-      technologies: ['HTML', 'CSS', 'Angular', 'Firebase'],
+         ◼️◼FrontEnd Architecture
+         ◼️◼Github Ci/CD
+         ◼️◼Bootstrapping apps with firebase.`.replaceAll('◼️', '<br>'),
+      technologies: ['Angular', 'JavaScript', 'CSS', 'Firebase', 'GitHub & Git'],
     },
     {
-      title: 'Portfolio Website',
+      title: 'Senior Front-End Consultant (Angular)',
+      employer: 'Saint-Gobain',
+      year: 'Apr 2022 - Apr 2023',
       image: this.project3,
       description:
-        'A personal portfolio website showcasing projects, skills, and contact information.',
-      technologies: ['HTML', 'CSS', 'React', 'Bootstrap'],
+        `◼Angular 10 ( 5 year old +100.000 ligne of code)
+        ◼️◼RXJS
+        ◼️◼ngrx
+        ◼️◼typescript
+        ◼️◼Java 11
+        ◼️◼Atlassina : jira, confluence
+        ◼️◼Jinkins`.replaceAll('◼️', '<br>'),
+      technologies: ['Angular', 'Java', 'Docker ', 'Maven'],
     },
     {
-      title: 'Blogging Platform',
+      title: 'Consultant Front-End Angular',
+      employer: 'ArcelorMitta',
+      year: 'Sep 2021 - Feb 2022',
       image: this.project4,
       description:
-        'A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.',
-      technologies: ['HTML', 'CSS', 'Vue.js', 'Express', 'mySQL'],
+        `◼NX project v12
+        ◼️◼Angular 13
+        ◼️◼Project : WebCare`.replaceAll('◼️', '<br>'),
+      technologies: ['Design d’interface utilisateur', 'CSS', 'Angular 13', 'Express', 'mySQL'],
     },
   ];
 
