1e651dc138f59895470e9512f28cfe24d3ab7e9e
BEN <ben94med@gmail.com>
2024-09-24 02:02:57 +0200

    MBY : stabilization after  merge with branch main

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,4 +2,6 @@
 export const environment = {
     production: false,
     apiUrl: 'http://localhost:3000',
+    googleTagManagerId: "GOOGLE-TAG-MANAGER-ID",
+    mapboxAccessToken: "MAP-BOX-ACCESS-TOKEN"
 };
