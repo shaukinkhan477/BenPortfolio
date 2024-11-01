28f8bd4b8e13dc89d57e2dc9234101beb0a5c1e8
shaukinkhan477 <shaukinkhan477@gmail.com>
2024-11-01 15:25:20 +0530

    correction in routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,16 @@
 import { RouterModule, Routes } from '@angular/router';
-
-import { AboutComponent } from './page-sections/about/about.component';
-import { ContactComponent } from './page-sections/contact/contact.component';
-import { ExperienceComponent } from './page-sections/experience/experience.component';
-import { HeroComponent } from './page-sections/hero/hero.component';
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
-import { ProjectsComponent } from './page-sections/projects/projects.component';
-import { ServicesComponent } from './page-sections/services/services.component';
-import { TechnologiesComponent } from './page-sections/technologies/technologies.component';
-import { TestimonialComponent } from './page-sections/testimonial/testimonial.component';
 import { BlogComponent } from './components/blog/blog.component';
 import { BlogDetailComponent } from './components/blog-detail/blog-detail.component';
+import { AboutComponent } from './home-page-sections/about/about.component';
+import { ContactComponent } from './home-page-sections/contact/contact.component';
+import { ExperienceComponent } from './home-page-sections/experience/experience.component';
+import { HeroComponent } from './home-page-sections/hero/hero.component';
+import { ProjectsComponent } from './home-page-sections/projects/projects.component';
+import { ServicesComponent } from './home-page-sections/services/services.component';
+import { TechnologiesComponent } from './home-page-sections/technologies/technologies.component';
+import { TestimonialComponent } from './home-page-sections/testimonial/testimonial.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -26,7 +25,6 @@ export const routes: Routes = [
   { path: 'testimonials', component: TestimonialComponent },
   { path: 'blog', component: BlogComponent },
   { path: 'blog/:id', component: BlogDetailComponent },
-  // Add more routes as needed
 ];
 
 @NgModule({
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,24 +1,23 @@
-import { AboutComponent } from '../page-sections/about/about.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ContactComponent } from '../page-sections/contact/contact.component';
 import { ContactModalComponent } from '../shared/contact-modal/contact-modal.component';
-import { ExperienceComponent } from '../page-sections/experience/experience.component';
 import { HeaderComponent } from '../components/header/header.component';
-import { HeroComponent } from '../page-sections/hero/hero.component';
-import { ProjectsComponent } from '../page-sections/projects/projects.component';
-import { ServicesComponent } from '../page-sections/services/services.component';
-import { TechnologiesComponent } from '../page-sections/technologies/technologies.component';
-import { TestimonialComponent } from '../page-sections/testimonial/testimonial.component';
-import { MyTeamComponent } from "../page-sections/my-team/my-team.component";
-import { FooterComponent } from "../components/footer/footer.component";
+import { HeroComponent } from "../home-page-sections/hero/hero.component";
+import { AboutComponent } from "../home-page-sections/about/about.component";
+import { TechnologiesComponent } from "../home-page-sections/technologies/technologies.component";
+import { ProjectsComponent } from "../home-page-sections/projects/projects.component";
+import { ServicesComponent } from "../home-page-sections/services/services.component";
+import { MyTeamComponent } from "../home-page-sections/my-team/my-team.component";
+import { TestimonialComponent } from "../home-page-sections/testimonial/testimonial.component";
+import { ContactComponent } from "../home-page-sections/contact/contact.component";
+
 
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, HeroComponent, AboutComponent, TechnologiesComponent, ExperienceComponent, ProjectsComponent, ContactComponent, TestimonialComponent, ServicesComponent, ContactModalComponent, MyTeamComponent, FooterComponent],
+  imports: [HeroComponent, AboutComponent, TechnologiesComponent, ProjectsComponent, ServicesComponent, ContactModalComponent, MyTeamComponent, TestimonialComponent, ContactComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
